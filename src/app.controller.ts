0ebf36f2fcfbafda177ac9988e03ebb7968eab2f
pviprey <viprey.pierre@gmail.com>
2024-03-13 16:16:32 +0100

    adding exemple

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,21 +6,25 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService, private ConfigurationService: ConfigurationService) {}
 
+  //example get request: localhost:3000/
   @Get()
   getHello(): string {
     return this.appService.getHello();
   }
 
+  //example get request: localhost:3000/mbappe
   @Get('/mbappe')
   getMbappe(): string {
     return this.appService.getMbappe();
   }
 
+  //example get request: localhost:3000/ooc
   @Get('/ooc')
   getOocRandom(): string {
     return this.getOOC(Math.floor(Math.random() * this.appService.OOC.length));
   }
 
+  //example get request: localhost:3000/ooc/1
   @Get('/ooc/:index')
   getOOC(@Param('index', ParseIntPipe) index: number): string {
     return this.appService.getOOC(index % this.appService.OOC.length);
